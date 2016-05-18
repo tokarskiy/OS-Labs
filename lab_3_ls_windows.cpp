1563414106e186d662b5bc3c67119d234b09213e
Andriy Tokarskiy <tokarskiy@users.noreply.github.com>
2016-05-18 14:16:47 +0300

    Changed task

--- lab_3_ls_windows.cpp
+++ lab_3_ls_windows.cpp
@@ -1,4 +1,17 @@
 
+
+/*
+ *	OS Lab #3.
+ *  	My 'ls' command 
+ *  	от 1024 до 2047 байт — в следующий интервал и т.д.).
+ *
+ *  	OS Windows
+ *
+ *	Author: Andriy Tokarskiy
+ *	IASA, DA-32, Variant #21
+ */
+
+
 #include <Windows.h>
 #include <tchar.h>
 #include <iostream>
@@ -7,30 +20,62 @@
 
 #define DIAGRAM_STEP 10000
 
+struct FileInfo {
+	std::string name;
+	bool isFolder;
+	bool readOnly;
+	bool hidden;
+	long size;
+	FILETIME creationTime;
+
+	void Print() {
+		SYSTEMTIME time, timeLocal;
+		FileTimeToSystemTime(&creationTime, &time);
+		SystemTimeToTzSpecificLocalTime(NULL, &time, &timeLocal);
+
+		printf("%30s %6s %8s %6s", name.c_str(), isFolder ? "FOLDER" : "------",
+							readOnly ? "READONLY" : "--------",
+							hidden ? "HIDDEN" : "------");
+		if (isFolder) {
+			printf(" %9s    ", "");
+		}
+		else {
+			printf(" %9ld    ", size);
+		}
+
+		printf("%2d/%2d/%d  %02d:%02d\n",
+			timeLocal.wDay, timeLocal.wMonth, timeLocal.wYear,
+			timeLocal.wHour, timeLocal.wMinute);
+
+	}
+};
+
+void PrintAll(std::vector<FileInfo>& files) {
+	std::vector<FileInfo>::iterator at = files.begin();
+	for (; at != files.end(); at++) {
+		at->Print();
+	}
+}
+
 void AddFilesInfoToVector(std::string directory, 
 		std::string filter, 
-		std::vector<std::string>& names,
-		std::vector<bool>& isDirectory);
+		std::vector<FileInfo>&);
 
 int main(int argc, char **argv) {
-	std::vector<std::string> fileNames;
+	std::vector<FileInfo> files;
 	std::vector<bool> isDirectory;
 
 	if (argc == 1) {
-		AddFilesInfoToVector(".\\", "*.*", fileNames, isDirectory);
+		AddFilesInfoToVector(".\\", "*.*", files);
 	}
 	else {
 		for (int i = 1; i < argc; i++) {
-			std::string current(argv[1]);
-			AddFilesInfoToVector(current + "\\", "*.*", fileNames, isDirectory);
+			std::string current(argv[i]);
+			AddFilesInfoToVector(current + "\\", "*.*", files);
 		}
 	}
-
 	
-
-	for (int i = 0; i < fileNames.size(); i++) {
-		printf("%40s %6s\n", fileNames[i].c_str(), isDirectory[i] ? "FOLDER" : "FILE");
-	}
+	PrintAll(files);
 
 	system("pause");
 	return 0;
@@ -39,8 +84,7 @@ int main(int argc, char **argv) {
 
 void AddFilesInfoToVector(std::string directory,
 		std::string filter,
-		std::vector<std::string>& names,
-		std::vector<bool>& isDirectory) {
+		std::vector<FileInfo>& files) {
 	WIN32_FIND_DATA fileData;
 	HANDLE fileHandle = FindFirstFile((directory + filter).c_str(), &fileData);
 
@@ -50,8 +94,14 @@ void AddFilesInfoToVector(std::string directory,
 			if (fileName == ".." || fileName == ".") {
 				continue;
 			}
-			isDirectory.push_back((bool)(fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY));
-			names.push_back(fileName);
+			FileInfo info;
+			info.isFolder = (bool)(fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
+			info.hidden = (bool)(fileData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN);
+			info.readOnly = (bool)(fileData.dwFileAttributes & FILE_ATTRIBUTE_READONLY);
+			info.name = fileName;
+			info.size = fileData.nFileSizeHigh * (MAXDWORD + 1) + fileData.nFileSizeLow;
+			info.creationTime = fileData.ftCreationTime;
+			files.push_back(info);
 		} while (FindNextFile(fileHandle, &fileData) != 0);
 		FindClose(fileHandle);
 	}
